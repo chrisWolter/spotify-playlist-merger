66aecf0b74a433cea80aaeea00315a753acfe0db
chriswolter <wolter-christian@outlook.com>
2020-08-04 22:21:01 +0200

    Init App (test)

---
+++ app.js
@@ -0,0 +1,44 @@
+var SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+});
+
+var request = require('request'); // "Request" library
+
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    spotifyApi.setAccessToken(token)
+    var options = {
+      url: 'https://api.spotify.com/v1/me/playlists',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
