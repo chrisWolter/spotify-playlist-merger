b06fd9f3e9985bf4e62906a5156e7821393de118
Andreas Partsch <public@partsch.xyz>
2020-09-16 22:50:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var SpotifyWebApi = require("spotify-web-api-node");
+const SpotifyWebApi = require("spotify-web-api-node");
 require("dotenv").config();
 
-var scopes = ['user-read-private', 'user-read-email'],
+const scopes = ['user-read-private', 'user-read-email'],
   redirectUri = process.env.REDIRECT_URI,
   clientId = process.env.CLIENT_ID,
   state = 'login';
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId: clientId,
   redirectUri: redirectUri,
 });
 
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+const authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
 console.log(authorizeURL);
