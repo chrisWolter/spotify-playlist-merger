0d1dc36fc99651de8fda3ec7de538cfef6e4a300
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2021-04-03 20:42:14 +0200

    Upgrade to vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "@/views/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans')
+body
+  font-family: 'Segoe UI, Work Sans', sans-serif !important
+</style>
