f88355bc41742db64589bb6145d24330e18d3958
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2021-04-03 20:42:41 +0200

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header text-center">
+        <h1 class="headline">Spotify Playlist Merger</h1>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.header
+    height: 150px
+    line-height: 150px
+    background-color: #1DB954
+    color: white
+    
+.headline
+    display: inline-block;
+    vertical-align: middle;
+    line-height: normal;
+    font-weight: bold
+
+@media only screen and (max-width: 767px)
+    .header
+        height: 75px
+        line-height: 75px
+    .headline
+        font-size: 24px    
+</style>
