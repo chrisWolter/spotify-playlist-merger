421c71ecfd25e6d3ae7abf65d49edbcd0c031c13
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2021-04-09 22:27:09 +0200

    clean up main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
@@ -8,10 +10,10 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 
-// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   render: (h) => h(App),
